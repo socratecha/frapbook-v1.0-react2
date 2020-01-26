9289df5ac12ea4d8834e21d2046fa5f5400160d5
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:30:35 -0500

    Controlled username input

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {username : ''};
         this.logIt = this.logIt.bind(this);
+        this.updateUsername = this.updateUsername.bind(this);
     }
     
     logIt(event) {
         event.preventDefault();
-        console.log('submit button clicked');
+        console.log('submit button clicked for user ' + this.state.username);
+    }
+
+    updateUsername(event) {
+        this.setState({username: event.target.value});
     }
     
     render() {
@@ -18,27 +24,7 @@ class App extends Component {
             <h1>Registration Form</h1>
             <form>
               Username&nbsp;
-              <input type="text"/>
-              <br/>
-              Password&nbsp;
-              <input type="password"/>
-              <br/>
-              Sign up for:&nbsp;
-              <select>
-                <option value="links">Links to New Videos (Free)</option>
-                <option value="news">Super-Valuable Newsletter ($29)</option>
-                <option value="course">Online Step-by-Step Course ($79)</option>
-                <option value="coach">One-on-One Expert Coaching ($149)</option>
-              </select><br/>
-              I am <strong>most</strong> interested in:&nbsp;
-              <input type="radio" name="interest" value="Justice"/>Justice&nbsp;
-              <input type="radio" name="interest" value="Freedom"/>Freedom&nbsp;
-              <input type="radio" name="interest" value="Equanimity"/>Equanimity<br/>
-              Please sign me up for your spam emails
-              <input type="checkbox"/>
-              <br/>
-              Provide an optional short bio:
-              <textarea rows="4" cols="50"/>
+              <input type="text" value={this.state.username} onChange={this.updateUsername}/>
               <br/>
               <button onClick={this.logIt}>Register</button> 
             </form> 
