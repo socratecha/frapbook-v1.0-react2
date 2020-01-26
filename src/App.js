f4f92df73547e2359d9887e70b3cc5a150716b35
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:30:19 -0500

    Basic sample form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.logIt = this.logIt.bind(this);
+    }
+    
+    logIt(event) {
+        event.preventDefault();
+        console.log('submit button clicked');
+    }
+    
+    render() {
+        return (
+            <div>
+            <h1>Registration Form</h1>
+            <form>
+              Username&nbsp;
+              <input type="text"/>
+              <br/>
+              Password&nbsp;
+              <input type="password"/>
+              <br/>
+              Sign up for:&nbsp;
+              <select>
+                <option value="links">Links to New Videos (Free)</option>
+                <option value="news">Super-Valuable Newsletter ($29)</option>
+                <option value="course">Online Step-by-Step Course ($79)</option>
+                <option value="coach">One-on-One Expert Coaching ($149)</option>
+              </select><br/>
+              I am <strong>most</strong> interested in:&nbsp;
+              <input type="radio" name="interest" value="Justice"/>Justice&nbsp;
+              <input type="radio" name="interest" value="Freedom"/>Freedom&nbsp;
+              <input type="radio" name="interest" value="Equanimity"/>Equanimity<br/>
+              Please sign me up for your spam emails
+              <input type="checkbox"/>
+              <br/>
+              Provide an optional short bio:
+              <textarea rows="4" cols="50"/>
+              <br/>
+              <button onClick={this.logIt}>Register</button> 
+            </form> 
+            </div>
+        );
+    }
 }
 
 export default App;
