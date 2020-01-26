df07d709b962162f9b10603e83e084955e7a0fdd
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:31:30 -0500

    Timer + callbacks

--- src/App.js
+++ src/App.js
@@ -2,30 +2,52 @@ import React, {Component} from 'react';
 import './App.css';
 
 class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {offset: this.props.localOffset};
+        this.alterOffset = this.alterOffset.bind(this);
+    }
+
+    alterOffset(event) {
+        event.preventDefault();
+        if (event.target.name === 'plus1') {
+            this.setState({offset: this.state.offset + 1});
+        } else if (event.target.name === 'minus1') {
+            this.setState({offset: this.state.offset - 1});
+        }
+        this.props.callback();
+    }
+    
     render() {
         return <div>
                  <h2>The local time</h2>
-                 <p>The time is now { this.props.universalTime + this.props.localOffset }</p>
-                 <p>for local time offset { this.props.localOffset }</p>
+                 <p>The time is now { this.props.universalTime + this.state.offset }</p>
+                 <p>for local time offset { this.state.offset }</p>
+                 <button onClick={ this.alterOffset } name='minus1'>-1</button>
+                 <button onClick={ this.alterOffset } name='plus1'>+1</button>
                </div>;
     }
 }
-    
+
 class App extends Component {
     constructor(props) {
-        super(props);
+        super(props);        
+        this.state = {time: 0, numPresses: 0};
+        this.pushed = this.pushed.bind(this);
         
-        this.state = {time: 0};
-        setInterval( () => this.setState({time: this.state.time + 1}), 1000);
+        setInterval( () => this.setState({time: this.state.time + 1}), 1000);        
     }
-        
+
+    pushed() { this.setState({numPresses:this.state.numPresses+1}); }
+    
     render() {
         return (
             <div>
               <h1>I like clocks</h1>
-              <Clock universalTime={ this.state.time} localOffset={ 5 } />
-              <Clock universalTime={ this.state.time} localOffset={ 100 } />
-              <Clock universalTime={ this.state.time} localOffset={ 200 } />
+              <Clock universalTime={ this.state.time} localOffset={ 5 }   callback={this.pushed}/>
+              <Clock universalTime={ this.state.time} localOffset={ 100 } callback={this.pushed}/>
+              <Clock universalTime={ this.state.time} localOffset={ 200 } callback={this.pushed}/>
+              <p>{ this.state.numPresses } button presses</p>
             </div>
         );
     }
